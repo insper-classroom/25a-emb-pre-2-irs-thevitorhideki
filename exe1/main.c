e2c96f594732d5b7613c8a39f0e105da8cda17f6
Vitor Katakura <7vitorgomez@gmail.com>
2025-02-16 18:11:56 -0300

    Exercício 1

--- exe1/main.c
+++ exe1/main.c
@@ -3,25 +3,34 @@
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
+volatile int pressed = 0;
+volatile int released = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
-    printf("fall \n");
-  } else if (events == 0x8) { // rise edge
-    printf("rise \n");
-  }
+    if (events == 0x4) { // fall edge
+        pressed = 1;
+    } else if (events == 0x8) { // rise edge
+        released = 1;
+    }
 }
 
 int main() {
-  stdio_init_all();
+    stdio_init_all();
 
-  gpio_init(BTN_PIN_R);
-  gpio_set_dir(BTN_PIN_R, GPIO_IN);
-  gpio_pull_up(BTN_PIN_R);
+    gpio_init(BTN_PIN_R);
+    gpio_set_dir(BTN_PIN_R, GPIO_IN);
+    gpio_pull_up(BTN_PIN_R);
 
-  gpio_set_irq_enabled_with_callback(
-      BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+    gpio_set_irq_enabled_with_callback(
+        BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
-  while (true) {
-  }
+    while (true) {
+        if (pressed) {
+            printf("fall\n");
+            pressed = 0;
+        } else if (released) {
+            printf("rise\n");
+            released = 0;
+        }
+    }
 }
